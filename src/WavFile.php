3ad9f97b9e90fd278f750584522b3c4471240d52
scorp7mix <scorp7mix@gmail.com>
2016-04-27 23:30:12 +0300

    feature/BBBREC-3 add pauses to sound output

--- src/WavFile.php
+++ src/WavFile.php
@@ -8,22 +8,36 @@ namespace ProfIT\Bbb;
  */
 class WavFile
 {
+    /**
+     * Описательная часть WAV файла
+     * HEADER - основная информация (тип, размер файла)
+     * FORMAT - описание звуковых характеристик (частота, битрейт, и т.п.)
+     * LIST - дополнительная информация
+     * DATA - сами данные
+     */
     protected $headerFormat = [
-        'chunkId'       => 'V',
-        'chunkSize'     => 'V',
-        'format'        => 'V',
-        'subchunk1Id'   => 'V',
-        'subchunk1Size' => 'V',
-        'audioFormat'   => 'v',
-        'numChannels'   => 'v',
-        'sampleRate'    => 'V',
-        'byteRate'      => 'V',
-        'blockAlign'    => 'v',
-        'bitsPerSample' => 'v',
-        'subchunk2Id'   => 'V',
-        'subchunk2Size' => 'V',
+        'headerGroupId'       => 'A4',
+        'headerFileSize'      => 'V',
+        'headerRiffType'      => 'A4',
+        'formatGroupId'       => 'A4',
+        'formatChunkSize'     => 'V',
+        'formatTag'           => 'v',
+        'formatChannels'      => 'v',
+        'formatSampleRate'    => 'V',
+        'formatByteRate'      => 'V',
+        'formatBlockAlign'    => 'v',
+        'formatBitsPerSample' => 'v',
+        'listGroupId'         => 'A4',
+        'listChunkSize'       => 'V',
+        'listType'            => 'A4',
+        'listData'            => 'A88',
+        'dataGroupId'         => 'A4',
+        'dataChunkSize'       => 'V',
     ];
     
+    const HEADER_OFFSET = 8;
+    const DATA_OFFSET = 144;
+
     protected $fileName;
     public $headers;
 
@@ -38,10 +52,11 @@ class WavFile
 
     /**
      * @param string $headersPacked - заголовки в сыром виде
-     * 
+     *
      * @return array - заголовки в виде ассоциативного массива
      */
-    public function unpackHeaders($headersPacked) {
+    protected function unpackHeaders($headersPacked)
+    {
         $unpackFormat = [];
         foreach ($this->headerFormat as $k => $v) {
             $unpackFormat[] = $v . $k;
@@ -54,7 +69,8 @@ class WavFile
      *
      * @return string - заголовки в сыром виде
      */
-    public function packHeaders($headers) {
+    protected function packHeaders($headers)
+    {
         $packFormat = implode('', $this->headerFormat);
         return call_user_func_array('pack', array_merge([$packFormat], $headers));
     }
@@ -64,14 +80,14 @@ class WavFile
      *
      * @throws \ProfIT\Bbb\Exception
      */
-    public function getHeadersPacked()
+    protected function getHeadersPacked()
     {
         $src = fopen($this->fileName, 'r');
         if (false === $src) {
             throw new \ProfIT\Bbb\Exception ('Ошибка открытия файла: ' . $this->fileName);
         }
 
-        $headersPacked = fread($src, 44);
+        $headersPacked = fread($src, self::DATA_OFFSET);
         fclose($src);
 
         return $headersPacked;
@@ -80,7 +96,7 @@ class WavFile
     /**
      * @return array - заголовки в виде ассоциативного массива
      */
-    public function getHeaders()
+    protected function getHeaders()
     {
         return $this->unpackHeaders($this->getHeadersPacked());
     }
@@ -119,6 +135,33 @@ class WavFile
 
         fclose($dst);
         fclose($src);
+    }
+
+    /**
+     * @param int $time - время в миллисекундах
+     *
+     * @return int - количество байт, соответствующее заданному времени
+     */
+    public function calculateBytesByTime($time) {
+        
+        $sampleRate = $this->headers['formatSampleRate'];
+        $channels = $this->headers['formatChannels'];
+        $bitsPerSample = $this->headers['formatBitsPerSample'];
+        
+        return (int)($sampleRate * $channels * ($bitsPerSample / 8) * ($time / 1000));
+    }
+    
+    /**
+     * @param string $dstFileName - путь к файлу, в который нужно записать данные
+     * @param int $pauseBytes - пауза в байтах
+     */
+    public function exportPause($dstFileName, $pauseBytes)
+    {
+        $dst = fopen($dstFileName, 'a');
+
+        fwrite($dst, str_repeat("\xff", $pauseBytes));
+
+        fclose($dst);
     }
 
 }
