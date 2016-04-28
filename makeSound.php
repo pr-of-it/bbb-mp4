4370f71aa24419b84dd09a749731059806771fa7
scorp7mix <scorp7mix@gmail.com>
2016-04-28 21:33:37 +0300

    feature/BBBREC-3 переделана сборка звука под sox

--- makeSound.php
+++ makeSound.php
@@ -1,61 +1,50 @@
 <?php
 /**
- * @use php makeSound.php --src=voice.events --dst=sound.wav
+ * @use php makeSound.php --src=voice.events --src-dir=./resources/audio --dst=sound.wav
  */
 require __DIR__ . '/autoload.php';
 
-$options = getopt('', ['src:', 'dst:']);
+define('SOX_PATH', 'sox');
+
+$options = getopt('', ['src:', 'src-dir:', 'dst:']);
 $srcFileName = realpath($options['src']);
-$dstFileName = $options['dst'];
+$srcDirName  = realpath($options['src-dir']);
+$dstFileName = realpath(dirname($options['dst'])) . DIRECTORY_SEPARATOR . basename($options['dst']);
 
-$events = new \ProfIT\Bbb\EventsFile($srcFileName);
-
-$sounds = [];
-try {
-    $fragments = $events->extractFragments(
-        '~<event.+eventname=".+RecordingEvent">~',
-        '~</event>~'
-    );
-
-    $index = 0;
-    $capture = false;
-    foreach ($fragments as $fragment) {
-        if (preg_match('~<event\s+timestamp="(\d+)".+eventname="StartRecordingEvent">~', $fragment, $m)) {
-            $sounds[$index]['start'] = (int)$m[1];
-            $capture = true;
-        }
-        if ($capture && preg_match('~<filename>(.*)</filename>~', $fragment, $m)) {
-            $sounds[$index]['fileName'] = $m[1];
-        }
-        if ($capture && preg_match('~<event\s+timestamp="(\d+)".+eventname="StopRecordingEvent">~', $fragment, $m)) {
-            $sounds[$index]['stop'] = (int)$m[1];
-            $capture = false;
-            $index++;
-        }
-    }
-
-    $headerOffset = \ProfIT\Bbb\WavFile::HEADER_OFFSET;
-    $dataOffset = \ProfIT\Bbb\WavFile::DATA_OFFSET;
-    $dataChunkSize = 0;
-
-    foreach ($sounds as $index => $sound) {
-
-        $wav = new \ProfIT\Bbb\WavFile($sound['fileName']);
-        if (0 !== $index) {
-            $pauseTime = $sound['start'] - $sounds[$index-1]['stop'];
-
-            $pauseBytes = $wav->calculateBytesByTime($pauseTime);
-            $dataChunkSize += $pauseBytes;
-            $wav->exportPause($dstFileName, $pauseBytes);
-        }
-
-        $dataChunkSize += $wav->headers['dataChunkSize'];
-        $wav->headers['dataChunkSize'] = $dataChunkSize;
-        $wav->headers['headerFileSize'] = $dataChunkSize + $dataOffset - $headerOffset;
-
-        $wav->exportHeaders($dstFileName);
-        $wav->exportData($dstFileName);
-    }
-} catch (\ProfIT\Bbb\Exception $e) {
-    echo $e->getMessage() . PHP_EOL;
+$src = fopen($srcFileName, 'r');
+if (false === $src) {
+    echo 'Ошибка открытия файла: ' . $srcFileName;
+    exit(0);
 }
+
+$fragments = [];
+
+while ($csv = fgetcsv($src, 1024)) {
+    var_dump($csv);
+    if ('Start' === $csv[0]) {
+
+        $fragmentSource = empty($srcDirName) ? $csv[2] : $srcDirName . DIRECTORY_SEPARATOR . basename($csv[2]);
+
+        if (isset($firstFragmentStart)) {
+            $fragmentDelay = ((int)$csv[1] - $firstFragmentStart) / 1000;
+
+            $mergePart = (0 === count($fragments)) ? '' : '- -m ';
+            $fragments[] = SOX_PATH . ' ' . $mergePart . $fragmentSource . ' -p pad ' . $fragmentDelay . ' 0 | ';
+        } else {
+            $firstFragmentStart = (int)$csv[1];
+            $firstFragmentSource = $fragmentSource;
+        }
+    }
+}
+
+fclose($src);
+
+if (isset($firstFragmentSource)) {
+    if (count($fragments) > 0) {
+        $execString = implode('', $fragments) . SOX_PATH . ' - -m ' . $firstFragmentSource . ' ' . $dstFileName;
+    } else {
+        $execString = SOX_PATH . ' ' . $firstFragmentSource . ' ' . $dstFileName;
+    }
+}
+
+exec($execString);
