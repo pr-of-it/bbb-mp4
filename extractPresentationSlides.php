40aafe1b7b1bbbad475c59480ee99a764c98f723
Nigm <po4ta.rus.63@yandex.ru>
2016-05-20 16:32:58 +0300

    feature/BBBREC-7

---
+++ extractPresentationSlides.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @use php extractPresentationSlides.php --src= --size= --save=./
+ */
+require __DIR__ . '/autoload.php';
+
+$options = getopt('', ['src:', 'size:', 'save:']);
+$srcSource = realpath($options['src']);
+$sizeImage = realpath($options['size']);
+$srcImageSave = realpath($options['save']);
+
+$src = fopen($srcSource, 'r');
+if (false === $src) {
+    echo 'Error while opening file: ' . $srcFileName . PHP_EOL;
+    exit(0);
+}
+
+while (false !== $src) {
+
+}
