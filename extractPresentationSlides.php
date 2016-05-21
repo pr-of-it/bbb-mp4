1fa45c6fe91fefc4784948c6ddc9bf6ea8ed62d2
Nigm <po4ta.rus.63@yandex.ru>
2016-05-21 19:37:26 +0300

    консольная команда

--- extractPresentationSlides.php
+++ extractPresentationSlides.php
@@ -7,15 +7,16 @@ require __DIR__ . '/autoload.php';
 $options = getopt('', ['source:', 'width:', 'height:', 'save:']);
 $sourceFilePath = realpath($options['source']);
 $width = $options['width'] ?? 1280;
-$height  = $options['height'] ?? 720;
+$height = $options['height'] ?? 720;
 $imageFilePath = $options['save'];
 
 if (file_exists($sourceFilePath)) {
 
-    if (false !== mkdir(__DIR__ . $imageFilePath)) {
-        $directory = realpath($imageFilePath);
-        exec('convert -density 150 ' . $sourceFilePath . ' -resize ' . $width . 'x' . $height . ' ' . $directory . '/slide.png');
+    if (!file_exists(__DIR__ . $imageFilePath)) {
+        mkdir(__DIR__ . $imageFilePath);
     }
+    $directory = realpath($imageFilePath);
+    exec('convert -density 150 ' . $sourceFilePath . ' -resize ' . $width . 'x' . $height . ' ' . $directory . '/slide.png');
 
 } else {
     echo 'File does not exist';
