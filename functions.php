1bc24afaa8c7297a222fdafce3bb96ffe3fc296f
Nigm <po4ta.rus.63@yandex.ru>
2016-05-25 13:38:13 +0300

    + function execute

--- functions.php
+++ functions.php
@@ -6,9 +6,23 @@
  */
 function halt($message = '')
 {
-    if (null !== $message) {
+    if (null != $message) {
         $message .= "\n";
+        fwrite(STDERR , $message);
     }
-    fwrite(STDERR , $message);
     exit(1);
+}
+
+/*
+ * Execute the console command and check the result of its execution
+ *
+ * @param string
+ */
+function execute(string $command) {
+    $string = escapeshellcmd($command);
+    exec($string, $output, $return_var);
+
+    if (0 !== $return_var) {
+        halt('File error');
+    }
 }
