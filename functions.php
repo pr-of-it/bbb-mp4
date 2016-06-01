d3d130195df83ad59759da291f4b5553fc698e96
Nigm <po4ta.rus.63@yandex.ru>
2016-06-01 20:10:13 +0300

    BBBREC-14 #1

--- functions.php
+++ functions.php
@@ -2,7 +2,7 @@
 /*
  * Output an error to stderr
  *
- * @param string
+ * @param string $message
  */
 function halt($message = null)
 {
@@ -17,7 +17,8 @@ function halt($message = null)
 /*
  * Executes the console command and checks the result of its execution
  *
- * @param string
+ * @param string $command
+ * @param string $output
  */
 function execute(string $command, $output = null)
 {
@@ -26,10 +27,38 @@ function execute(string $command, $output = null)
     if (null !== $output) {
         $command .= ' > ' . $output;
     }
-    
+
     passthru($command, $code);
 
     if (0 !== $code) {
         halt('Fail executing console command. Exit status #' . $code);
     }
+}
+
+function extractCsv($dataPathFile, array $fieldNames = null)
+{
+    $file = fopen($dataPathFile, 'r');
+
+    if (false === $file) {
+        halt('Unable to open file for reading');
+    }
+
+    $data = [];
+    $mass = [];
+    while ($csv = fgetcsv($file, 1024)) {
+
+        if (null === $fieldNames) {
+            $data[] = $csv;
+        }
+
+        if (null !== $fieldNames) {
+            foreach ($fieldNames as $key => $value) {
+                $mass[$value] = $csv[$key];
+            }
+            $data[] = $mass;
+        }
+    }
+
+    fclose($file);
+    return $data;
 }
