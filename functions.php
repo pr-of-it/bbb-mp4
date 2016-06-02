ed874eb2dbabfc7fc7b4b6c28d7d0057c871d139
Nigm <po4ta.rus.63@yandex.ru>
2016-06-02 15:20:18 +0300

    BBBREC-14 #4

--- functions.php
+++ functions.php
@@ -20,17 +20,51 @@ function halt($message = null)
  * @param string $command
  * @param string $output
  */
-function execute(string $command, $output = null)
+function execute(string $command, string $output = null)
 {
     $command = escapeshellcmd($command);
 
     if (null !== $output) {
         $command .= ' > ' . $output;
     }
-    
+
     passthru($command, $code);
 
     if (0 !== $code) {
         halt('Fail executing console command. Exit status #' . $code);
     }
+}
+
+/*
+ * Extracts data from a CSV-file and returns an array of CSV data elements
+ *
+ * @param string $src
+ * @param array $fieldNames
+ *
+ * @return array
+ */
+function extractCSV(string $src, array $fieldNames = null)
+{
+    if (!is_readable($src)) {
+        halt('File with CSV data does not exist or is not readable');
+    }
+
+    $file = fopen($src, 'r');
+
+    $data = [];
+    while ($csv = fgetcsv($file, 1024)) {
+
+        if (null === $fieldNames) {
+            $data[] = $csv;
+        } else {
+            $row = [];
+            foreach ($fieldNames as $key => $value) {
+                $row[$value] = $csv[$key];
+            }
+            $data[] = $row;
+        }
+    }
+    
+    fclose($file);
+    return $data;
 }
