9a1197e5fe63599eb56d266a6d660b3de2211b0f
Nigm <po4ta.rus.63@yandex.ru>
2016-06-10 15:35:00 +0300

    BBBREC-17 #4

--- extractUserEvents.php
+++ extractUserEvents.php
@@ -8,6 +8,10 @@ require __DIR__ . '/functions.php';
 $options = getopt('', ['src:']);
 $srcFileName = realpath($options['src']);
 
+if (!is_readable($srcFileName)) {
+    halt('File does not exist or is not readable');
+}
+
 $events = new \ProfIT\Bbb\EventsFile($srcFileName);
 
 try {
@@ -19,16 +23,14 @@ try {
     foreach ($fragments as $fragment) {
         $eventParams = [];
 
-        if (preg_match('~<event\s+timestamp="(\d+)".+eventname="Participant(\w+)Event">~', $fragment, $m)) {
+        preg_match('~<event\s+timestamp="(\d+)".+eventname="Participant(\w+)Event">~', $fragment, $m);
             $eventParams[0] = $m[2];
             $eventParams[1] = $m[1];
-        }
-        if (preg_match('~<userId>(\w+)</userId>~', $fragment, $m)) {
+        preg_match('~<userId>(\w+)</userId>~', $fragment, $m);
             $eventParams[2] = $m[1];
-        }
-        if (preg_match('~<name>(.+)</name>~u', $fragment, $m)) {
+        preg_match('~<name>(.+)</name>~u', $fragment, $m);
             $eventParams[3] = $m[1];
-        }
+        
         if ('Left' == $eventParams[0]) {
             $eventParams[3] = '';
         }
