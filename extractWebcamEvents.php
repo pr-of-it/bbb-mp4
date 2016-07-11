40e1e51c4ed8df1b1aec5145f033476d8c51f4c7
Nigm <po4ta.rus.63@yandex.ru>
2016-07-11 16:21:06 +0300

    BBBREC-25 #1

---
+++ extractWebcamEvents.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * @use php extractWebcamEvents.php --src=events.xml > webcam.events
+ */
+require __DIR__ . '/autoload.php';
+require __DIR__ . '/functions.php';
+
+$options = getopt('', ['src:']);
+$srcFileName = realpath($options['src']);
+
+if (!is_readable($srcFileName)) {
+    halt('File does not exist or is not readable');
+}
+
+$events = new \ProfIT\Bbb\EventsFile($srcFileName);
+
+try {
+    $fragments = $events->extractFragments(
+        '~<event.+module="WEBCAM"\s+eventname="(Start|Stop)WebcamShareEvent">~',
+        '~</event>~'
+    );
+
+    foreach ($fragments as $fragment) {
+        $eventParams = [];
+
+        preg_match('~<event\s+timestamp="(\d+)".+eventname="(\w+)WebcamShareEvent">~', $fragment, $m);
+        $eventParams[0] = lcfirst($m[2]);
+        $eventParams[1] = $m[1];
+        preg_match('~<stream>(.+)</stream>~', $fragment, $m);
+        $eventParams[2] = $m[1];
+        fputcsv(STDOUT, $eventParams);
+    }
+} catch (\ProfIT\Bbb\Exception $e) {
+    halt($e->getMessage() . PHP_EOL);
+}
